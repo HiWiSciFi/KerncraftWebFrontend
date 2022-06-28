30031e65dc4d29e15ce41090b173a68ef8773e97
HiWiSciFi <19999080+HiWiSciFi@users.noreply.github.com>
2022-06-28 17:12:17 +0200

    added line

--- src/components/AnalyzeButton.svelte
+++ src/components/AnalyzeButton.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Line from "svelte-chartjs/src/Line.svelte"
 	import { API_HOST } from '../AppInfo';
 
 	let errorText: string = "";
@@ -38,6 +39,41 @@
 		outputWindow.style.height = "auto";
 		outputWindow.style.height = outputWindow.scrollHeight + "px";
 	}
+
+	let lineData = {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+};
 </script>
 
 <button style="font-size: 2em;" on:click={analyze}>Analyze</button>
@@ -49,6 +85,8 @@
 {#if outputText != ""}
 <br/><textarea bind:this={outputWindow} readonly class="output-text">{outputText}</textarea>
 {/if}
+
+<Line data={lineData} />
 
 <style lang="scss">
 	@import '../styles/button';
